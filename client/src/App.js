6d42b23c003448ecfb1f05da7aebaf66b14c5730
Łukasz Dawidowicz <lddev@outlook.com>
2020-05-19 16:02:25 +0200

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/views/Home/Home';
