dc038402bce77a22c202869ffcae06de6bd11d74
Łukasz Dawidowicz <lddev@outlook.com>
2020-05-19 11:50:19 +0200

    initial commit

---
+++ client/src/components/layout/MainLayout/Layout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../../styles/global.scss';
+
+const Layout = ({ title, description, className, children }) => {
+  return (
+    <div>
+      <div className='jumbotron'>
+        <h2>{title}</h2>
+        <p className='lead'>{description}</p>
+      </div>
+
+      <main className={className}>{children}</main>
+    </div>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  className: PropTypes.any,
+};
